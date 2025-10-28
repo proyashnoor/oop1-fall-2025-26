bce2ef7c21dfe6331fa575872cc98af0d7c4b547
proyashnoor <proyashnoor>
2025-10-28 17:31:00 +0600

    25-61907-2

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+    public static void main (String[] args){
+        byte x = 70;
+        byte y = 80;
+        byte z = 90;
+        int sum = x +y + z;
+        System.out.println("Sum is :" + sum + "Average is : " + (sum/3));
+
+
+    }
+    
+}
---
+++ Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        float C = 35.7f;
+        float F = 32f + (C*(180/100f));
+        System.out.println(" IN Celcius: " + C + " Degree = " + F + " In Farheinit ");
+    }
+
+    
+}
