1672f51506b4bc23f170cf27490e3adf3c5b9556
proyashnoor <proyashnoor>
2025-11-02 22:12:50 +0600

    removed

--- Task3.java
+++
@@ -1,9 +0,0 @@
-public class Task3 {
-    public static void main(String[] args) {
-        float C = 35.7f;
-        float F = 32f + (C*(180/100f));
-        System.out.println(" IN Celcius: " + C + " Degree = " + F + " In Farheinit ");
-    }
-
-    
-}
