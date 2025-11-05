7305e26fc6a8d87f42cb44c491ecdb01a3a9615d
proyashnoor <proyashnoor>
2025-11-06 00:49:59 +0600

    25-61907-2

--- Project/MortgageCalculator.java
+++ Project/MortgageCalculator.java
@@ -1,10 +1,11 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 public class MortgageCalculator {
 public static void main(String[] args) {
 
 Scanner scanner = new Scanner(System.in);
 System.out.println( "=== Bangladesh Mortgage Calculator (BDT) ===");
- // Amount
+
  System.out.println(" Enter loan amount (Principal in BDT) : ");
  double loanAmount = scanner.nextDouble();
 
@@ -14,14 +15,31 @@ System.out.println( "=== Bangladesh Mortgage Calculator (BDT) ===");
  System.out.println( " Enter loan period (in years): ");
  int years = scanner.nextInt();
 
+ double monthlyrate = ( annualInterest /100 )/12;
+ int months = years*12;
+ double monthlypayment = loanAmount * (monthlyrate*Math.pow((1 + monthlyrate),months))/
+ ((Math.pow((1 +monthlyrate),months))-1);
+
+ double totalpayment = monthlypayment * months;
+ double totalinterest = totalpayment-loanAmount;
+
+ NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+
  System.out.println( "=== Mortgage Summary ===");
 
- // Calculation
  double monthlyInterest = annualInterest/12;
  int numberOfpayments = years*12;
+
+ System.out.println(" Loan Amount: " + currencyFormatter.format(monthlypayment));
+
+ System.out.println( " MonthlyPayment : " + currencyFormatter.format(monthlypayment));
+
+ System.out.println(" Total Payment : " +  currencyFormatter.format(totalpayment));
+
+System.out.println(" Total Interest : " + currencyFormatter.format(totalinterest));
+
+scanner.close();
  
- // Payment Formula 
-//double monthlyPayment = 
 }
     
 }
